209e22e2c8c70204ad0a8673154d3f34e5679f9a
Paul <44707149+Paul1465@users.noreply.github.com>
2019-02-05 09:45:55 +0100

    database add

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-
-var_dump($_GET);
-var_dump($_POST);  
-
-//Si pseudo égale à admin et password égale à admin
-if ($_POST["pseudo"] === "pseudo" && $_POST["password"] === "password"){
-    echo "Bonjour " .$_POST["pseudo"];
-
-
-    
-
-//Sinon si wrong 
-}else if($_POST["password"] !== "password"){
-header("Location: login.php?message=Erreur de password");
-}else if ($_POST["pseudo"] !== "pseudo"){
-    header("Location: login.php?message=Erreur de pseudo");
-}
+session_start();
+require 'nav-bar.php';
+require 'database.php';
  
 
 ?>
